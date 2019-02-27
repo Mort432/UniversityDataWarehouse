cee7c5fc106bbb1ee9c20b90601dc057c45ccce4
Mort432 <connaghmuldoon@gmail.com>
2019-02-27 20:11:06 +0000

    Initial commit.

---
+++ UniversityDataWarehouse.WPF/MainWindow.xaml.cs
@@ -0,0 +1,13 @@
+﻿namespace UniversityDataWarehouse.WPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
